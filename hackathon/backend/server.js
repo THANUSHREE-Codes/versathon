d25f8e10c8743a401f2a4d6efbafee9890b9ffbe
THANUSHREE <uthanushree2006@gmail.com>
2026-02-15 07:54:09 +0530

    3rd commit changed Solidity source code files

---
+++ hackathon/backend/server.js
@@ -0,0 +1,5 @@
+// HTMLAreaElement is a built-in interface in JavaScript that represents an HTML <area> element. The <area> element is used within an <map> element to define clickable areas on an image. The HTMLAreaElement interface provides properties and methods to manipulate and interact with these area elements.
+
+// Here's an example of how to use the HTMLAreaElement interface in JavaScript:```javascript
+// Get a reference to the area element
+// const areaElement = document.querySelector('area'); 
---
+++ hackathon/backend/utils/hash.js
@@ -0,0 +1,6 @@
+// HashChangeEvent is a built-in event in JavaScript that is fired when the fragment identifier of the URL (the part after the # symbol) changes. This event is commonly used in single-page applications (SPAs) to detect changes in the URL and update the content accordingly without reloading the page.
+
+// Here's an example of how to use the HashChangeEvent in JavaScript:```javascript// Listen for the hashchange event
+// window.addEventListener('hashchange', function() {
+//     console.log('The hash has changed to: ' + window.location.hash);
+// });     
